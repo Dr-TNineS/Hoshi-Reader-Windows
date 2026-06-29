ab8c5532907dbc40f45bdda42e61946a3e5cdad5
shishamo <506997957@qq.com>
2026-06-29 14:15:21 +0800

    fix: ignore tauri build output in vite watcher

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,11 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 export default defineConfig({
   plugins: [svelte()],
   base: "./",
+  server: {
+    watch: {
+      ignored: ["**/src-tauri/target*/**"],
+    },
+  },
   build: {
     target: "esnext",
   },
